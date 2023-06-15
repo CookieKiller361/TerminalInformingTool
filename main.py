38500e5cfed2bd7431012f86ee4c05fdc7dd4bb1
CookieKiller361 <jannis1lauerwald@gmail.com>
2023-06-15 18:39:55 +0200

    adding structure and code to the main menu to integrate the setup location function

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 from menu import menu
+import informations as info
 
-main_menu=menu('Test',"test1","Test2","Test3")
-print(f'{type(main_menu)} {main_menu}')
+main_menu=menu('Main',"Weather","Test2","Test3")
+
+if main_menu==1:
+    weather_data=info.get_weather_informations_continent("/europa/EU.html")
+    Weather_menu=menu('Weather',"Refresh Data","Setup Location","Load Weather")
+    if Weather_menu==1:
+        pass
+        #if statement looking if data exist 
+    elif Weather_menu==2:
+        weather_location_data=info.get_weather_informations_continent("/europa/EU.html")
+        info.setup_location(weather_location_data)
