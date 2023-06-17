8b06d1fecc27cc644128cc8dd9c6752aaa0bb88b
CookieKiller361 <jannis1lauerwald@gmail.com>
2023-06-17 21:31:20 +0200

    added functions to handle with the save data

--- TerminalInformingTool/file_handeling.py
+++ TerminalInformingTool/file_handeling.py
@@ -1,31 +1,47 @@
 import os
 import pandas as pd
-import csv
 
-#i want to implement a function like that directly in the setup_locaten function
-def save_data_create(file_location,filename,data):
-    #checks witch file system is used
-    if os.name('nt'):
-        full_path=file_location+"\\"+filename
+
+def check_if_exist(filepath):
+    if os.path.exists(filepath):
+        return True
     else:
-        full_path=file_location+"/"+filename
-    #checks if the file exists
-    
-    if os.path.exists(full_path):
-        '''
-        search_data_csv(full)
-        '''
-        file=open(full_path,"a")
-        file
-    else:
-        file=open(full_path,'w')
-        file.write(data)
-        file.close
-    
-    def search_data_csv(filepath,search_for):
-        data=pd.read_csv(filepath)
-        file=open(filepath,"r")
-        csv_reader=csv.reader(file)
-        for row in csv_reader:
-            if search_for in row:
-                return(row)
+        return False
+
+
+def csv_file_frame_create(filepath,header_data=None):
+    #creates a DataFrame from the data given within the header of the function
+    df = pd.DataFrame([header_data])
+    #saves the csv file with the data from the function header as header for the csv file
+    df.to_csv(filepath, index=False, header=False)
+
+
+def csv_file_add_data(filepath,data):
+    file=open(filepath,'a')
+    for key, value in data.items():
+        file.write(f'\n{key},{value}')
+    file.close
+
+
+def search_saved_data(filepath, column_name, search_value):
+    df = pd.read_csv(filepath)
+
+    # search for the value in the searched columm
+    index = df[df[column_name] == search_value].index[0]
+
+    # access the data from the next columm
+    next_column_value = df.iloc[index, df.columns.get_loc(column_name) + 1]
+
+    #returns the data from the next columm
+    return next_column_value
+
+
+def replace_in_csv(filepath, column_name, search_value, replace_data):
+    # CSV-Datei einlesen
+    df = pd.read_csv(filepath)
+
+    #search for the value of the choosen columm
+    df.loc[df[column_name] == search_value, column_name] = replace_data
+
+    # saves the changed csv file
+    df.to_csv(filepath, index=False)
